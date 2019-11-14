810b1a450cb6dcb0514729226ab60ff55d3596e2
piksenia <xenia.billinger@mt-ag.com>
2019-11-14 05:39:19 +0100

    #ADDING Class Person (POJO)

---
+++ mysite/model/Person.py
@@ -0,0 +1,10 @@
+class Person:
+    #name:str
+    #alter:int
+    #gewicht:complex
+
+    # Initializer / Instance Attributes
+    def __init__(self, name, age, weight):
+        self.name = name
+        self.age = age
+        self.weight = weight
